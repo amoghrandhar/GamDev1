4b1e0c895216a04449ee977ee9862d042a86b2b3
amoghrandhar <ar24g13@soton.ac.uk>
2016-02-28 18:01:16 +0000

    Moving Asteroids
    Moving them behind

---
+++ Assets/_scripts-asteroid/Mover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Mover : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    rb = this.GetComponent<Rigidbody>();
+	    rb.velocity = -1 * transform.forward*speed; // -ve as we want it to move down
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
