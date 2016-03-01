e99f44307989235df4a9729470b350cea7f21ba0
amoghrandhar <ar24g13@soton.ac.uk>
2016-03-01 17:42:27 +0000

    Adding a random Asteroid

---
+++ Assets/Gamecontroller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gamecontroller : MonoBehaviour {
+
+    public GameObject asteroid;
+    public Vector3 spawnValues;
+
+    // Use this for initialization
+    void Start () {
+        SpawnWaves();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //This will create random asteroids at random position 
+    void SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(asteroid, spawnPosition, spawnRotation);
+        Mover ast = asteroid.GetComponent<Mover>();
+        ast.speed = 5;
+        SpriteRenderer rendererCol = asteroid.GetComponentInChildren<SpriteRenderer>();
+        rendererCol.color = new Color(Random.value , Random.value , Random.value, 1f); // Set to opaque black
+
+
+    }
+
+}
--- Assets/_scripts-asteroids/Mover.cs
+++ Assets/_scripts-asteroids/Mover.cs
@@ -4,19 +4,16 @@ using UnityEditor;
 
 public class Mover : MonoBehaviour
 {
-    private Rigidbody rb;
     public float speed;
 
     // Use this for initialization
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
-        rb.velocity = -1 * transform.forward * speed; // -ve as we want it to move down
     }
 
-    // Update is called once per frame
+    // Update is called once per frame -> Here we are moving the asteroid and rotating as well
     void Update()
     {
-		
+		transform.position += -speed*Vector3.forward * Time.deltaTime ;
     }
 }
